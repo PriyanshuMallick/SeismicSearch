9e309378e060d61b1f81bd33952cacdb3931785f
PriyanshuMallick <76875358+PriyanshuMallick@users.noreply.github.com>
2023-05-19 00:02:15 +0530

    + Get and Ingest data from USGR API 🌍🌏🌎

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,9 +1,12 @@
 import express from "express";
-import esClient, { test } from "./elasticsearch/client.js";
+import client, { test } from "./elasticsearch/client.js";
+import data from "./data_management/retrieve_and_ingest_data.js";
 
 const app = express();
 
 const port = 3001;
+
+app.use("/ingest_data", data);
 
 app.listen(port, () =>
   console.log(`Server listening at http://localhost:${port}`)
